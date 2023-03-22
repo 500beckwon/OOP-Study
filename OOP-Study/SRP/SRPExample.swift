5127945c5a59114796040165144a93e270da2223
500beckwon <kjsx60@gmail.com>
2023-03-22 21:54:21 +0900

    Git 예제 코드 사용

---
+++ OOP-Study/SRP/SRPExample.swift
@@ -0,0 +1,64 @@
+//
+//  SRPExample.swift
+//  OOP-Study
+//
+//  Created by ByungHoon Ann on 2023/03/22.
+//
+
+import Foundation
+protocol Openable {
+    mutating func open()
+}
+
+protocol Closeable {
+    mutating func close()
+}
+
+// 문. 캡슐화된 상태를 갖고 있으며 메서드를 사용해 변경할 수 있다.
+struct PodBayDoor: Openable, Closeable {
+
+    private enum State {
+        case open
+        case closed
+    }
+
+    private var state: State = .closed
+
+    mutating func open() {
+        state = .open
+    }
+
+    mutating func close() {
+        state = .closed
+    }
+}
+
+// 여는 일만 담당하며 안에 무엇이 들어있는 지, 어떻게 닫는 지 모른다.
+final class DoorOpener {
+    private var door: Openable
+
+    init(door: Openable) {
+        self.door = door
+    }
+
+    func execute() {
+        door.open()
+    }
+}
+
+// 닫는 일만 담당하며 안에 무엇이 들어있는 지, 어떻게 여는 지 모른다.
+final class DoorCloser {
+    private var door: Closeable
+
+    init(door: Closeable) {
+        self.door = door
+    }
+
+    func execute() {
+        door.close()
+    }
+}
+
+
+
+
