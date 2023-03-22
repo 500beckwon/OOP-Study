d80f7c1abcb027936a08b738a8b82178a2323c3e
500beckwon <kjsx60@gmail.com>
2023-03-22 21:54:31 +0900

    SRP 실사용

--- OOP-Study/ViewController.swift
+++ OOP-Study/ViewController.swift
@@ -14,5 +14,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func srpTest() {
+        let door = PodBayDoor()
+        // ⚠️ `DoorOpeneer`만이 문을 여는 책임이 있다.
+        let doorOpener = DoorOpener(door: door)
+        doorOpener.execute()
+
+        // ⚠️ 문을 닫은 후 다른 작업을 해야 하는 경우,
+        // 알람을 켜는 것처럼 `DoorOpener` 클래스를 변경할 필요가 없다.
+        let doorCloser = DoorCloser(door: door)
+        doorCloser.execute()
+    }
 }
 
